28de13c44eb441a7c13291b5cd6f5fd5086ea427
Jazer <saltanhayrliev2006@gmail.com>
2024-09-25 01:38:57 +0300

    decode is done

--- Window2.xaml.cs
+++ Window2.xaml.cs
@@ -19,10 +19,70 @@ namespace morseCode
     /// </summary>
     public partial class Window2 : Window
     {
+        private string txt;
+        private Dictionary<string, string> morse = new Dictionary<string, string>()
+        {
+            { "a",".-" },
+            { "b", "-..."},
+            { "c", "-.-."},
+            {"d", "-.."},
+            { "e", "." },
+            { "f", "..-." },
+            { "g", "--."},
+            { "h", "...."},
+            { "i", ".."},
+            { "j", ".---" },
+            { "k", "-.-"},
+            { "l", ".-.."},
+            { "m", "--"},
+            { "n", "-."},
+            { "o", "---"},
+            { "p", ".--."},
+            { "q", "--.-"},
+            { "r", ".-."},
+            { "s", "..."},
+            { "t", "-"},
+            { "u", "..-"},
+            { "v", "...-"},
+            { "w", ".--"},
+            { "x", "-..-"},
+            { "y", "-.--"},
+            { "z", "--.."},
+            { "1", ".----"},
+            { "2", "..---"},
+            { "3", "...--"},
+            { "4", "....-"},
+            { "5", "....."},
+            { "6", "-...."},
+            { "7", "--..."},
+            { "8", "---.."},
+            { "9", "----."},
+            { "0", "-----"}
+        };
         public Window2()
         {
             InitializeComponent();
+            myTextBox.TextChanged += MyTextBox_TextChanged;
         }
+
+        private void MyTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txt = myTextBox.Text;
+            string newTxt = "";
+            char[] arr = txt.ToCharArray();
+            for (int i = 0; i < arr.Length; i++) {
+                string letter = arr[i].ToString();
+                if (morse.ContainsKey(letter)) {
+                    newTxt += " "+morse[letter];
+                }
+                else
+                {
+                    newTxt += letter;
+                }
+            }
+            Eng.Text = newTxt;
+        }
+
         private void myTextBox_GotFocus(object sender, RoutedEventArgs e)
         {
             if (myTextBox.Text == "Введите текст...")
