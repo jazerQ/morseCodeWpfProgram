f5ef5ada84e2a094e80c5069af07fb6df260b3df
Jazer <saltanhayrliev2006@gmail.com>
2024-09-24 23:52:02 +0300

    add new window to code

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,14 @@ namespace morseCode
         public MainWindow()
         {
             InitializeComponent();
+            code.Click += Button_Click;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window1 = new Window1();
+            window1.Show();
+            this.Close();
         }
     }
 }
